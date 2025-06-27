ba1ff740768613dc3f89298fcc46f360f4e6c067
Joel Jo <108486336+ThaGreatJoel@users.noreply.github.com>
2025-06-27 10:14:20 +0530

    Update PasswordVault.js

--- models/PasswordVault.js
+++ models/PasswordVault.js
@@ -1,25 +1,23 @@
 const mongoose = require("mongoose")
+
 const passwordVaultSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true
+  },
+  site: {
+    type: String,
+    required: true
+  },
+  encryptedPassword: {
+    type: String,
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+})
 
-userId: {
-     type: mongoose.Schema.Types.ObjectId,
-        ref: "User",
-        required: true
-},
-                  
- site: {
-     type: String,
-     required: true
-},
-                            
-encryptedPassword: {
-     type: String,
-      required: true
-},
-                                        
-createdAt: {
-     type: Date,
-     default: Date.now
-}})
-
- module.exports = mongoose.model("PasswordVault", passwordVaultSchema)
+module.exports = mongoose.model("PasswordVault", passwordVaultSchema)
