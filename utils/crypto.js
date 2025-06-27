d2ba8cbd7b6c9d549c1f865c1608bb0856ba1f67
Joel Jo <108486336+ThaGreatJoel@users.noreply.github.com>
2025-06-27 10:15:10 +0530

    Update crypto.js

--- utils/crypto.js
+++ utils/crypto.js
@@ -5,19 +5,19 @@ const secretKey = process.env.ENCRYPTION_KEY
 const ivLength = 16
 
 function encrypt(text) {
-    const iv = crypto.randomBytes(ivLength)
-    const cipher = crypto.createCipheriv(algorithm, Buffer.from(secretKey), iv)
-    const encrypted = Buffer.concat([cipher.update(text), cipher.final()])
-    return iv.toString("hex") + ":" + encrypted.toString("hex")
+  const iv = crypto.randomBytes(ivLength)
+  const cipher = crypto.createCipheriv(algorithm, Buffer.from(secretKey), iv)
+  const encrypted = Buffer.concat([cipher.update(text), cipher.final()])
+  return iv.toString("hex") + ":" + encrypted.toString("hex")
 }
 
 function decrypt(hash) {
-    const [ivHex, encryptedHex] = hash.split(":")
-    const iv = Buffer.from(ivHex, "hex")
-    const encryptedText = Buffer.from(encryptedHex, "hex")
-    const decipher = crypto.createDecipheriv(algorithm, Buffer.from(secretKey), iv)
-    const decrypted = Buffer.concat([decipher.update(encryptedText), decipher.final()])
-    return decrypted.toString()
+  const [ivHex, encryptedHex] = hash.split(":")
+  const iv = Buffer.from(ivHex, "hex")
+  const encryptedText = Buffer.from(encryptedHex, "hex")
+  const decipher = crypto.createDecipheriv(algorithm, Buffer.from(secretKey), iv)
+  const decrypted = Buffer.concat([decipher.update(encryptedText), decipher.final()])
+  return decrypted.toString()
 }
 
- module.exports = { encrypt, decrypt }
+module.exports = { encrypt, decrypt }
