26c6fe22618a9ed7e8b2d500d3732b7df600b803
Joel Jo <108486336+ThaGreatJoel@users.noreply.github.com>
2025-06-27 10:12:17 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,28 +1,16 @@
 const mongoose = require("mongoose")
-const bcrypt = require("bcrypt")
+const bcrypt = require("bcryptjs")
 
 const userSchema = new mongoose.Schema({
-  username: {
-      type: String,
-      required: true, 
-      unique: true
-                },
-             
-   email: {
-       type: String,
-       required: true,
-      unique: true
-             },                    
+  username: { type: String, required: true, unique: true },
+  email:    { type: String, required: true, unique: true },
+  hashedPassword: { type: String, required: true }
+})
 
-       hashedPassword: {
-           type: String,
-         required: true
-                   }
-                      })
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("hashedPassword")) return next()
+  this.hashedPassword = await bcrypt.hash(this.hashedPassword, 10)
+  next()
+})
 
-  userSchema.pre("save", async function (next) {
-   if (!this.isModified("hashedPassword")) return next()
-    this.hashedPassword = await bcrypt.hash(this.hashedPassword, 10)
-             next()
-                      })
-      module.exports = mongoose.model("User", userSchema)
+module.exports = mongoose.model("User", userSchema)
